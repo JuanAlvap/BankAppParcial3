e0968081b9edc4c6d3ae21f09abbf38cf8837d01
JuanAlvap <alvarezjp@uninorte.edu.co>
2024-11-20 18:05:35 -0500

    Single Responsability of Account Controller

--- src/core/controllers/account/AccountController.java
+++ src/core/controllers/account/AccountController.java
@@ -4,14 +4,11 @@ import core.controllers.account.validate.AccountExistIdValidate;
 import core.controllers.account.validate.AccountIdValidate;
 import core.controllers.account.validate.AccountInitialBalanceValidate;
 import core.controllers.account.validate.AccountStorageValidate;
-import core.controllers.user.UserController;
 import core.controllers.utils.Response;
 import core.controllers.utils.Status;
 import core.models.Account;
-import core.models.User;
 import core.models.storage.AccountStorage;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class AccountController {
 
@@ -37,7 +34,7 @@ public class AccountController {
                 return new Response("Id must be numeric", Status.BAD_REQUEST);
             }
 
-            if (!storageValidate.storageValidate(initialBalanceText)) {
+            if (!storageValidate.storageValidate(userIdText,initialBalanceText)) {
                 return new Response("A user with that id already exists", Status.BAD_REQUEST);
             }
             return new Response("User register succesfully", Status.CREATED);
--- src/core/controllers/account/validate/AccountExistIdValidate.java
+++ src/core/controllers/account/validate/AccountExistIdValidate.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package core.controllers.account.validate;
 
 import core.controllers.user.UserController;
 import core.models.User;
 
-/**
- *
- * @author Juanpi
- */
 public class AccountExistIdValidate {
 
     public AccountExistIdValidate() {
