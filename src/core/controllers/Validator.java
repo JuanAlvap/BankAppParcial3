5d12953c6e58d8b91e2a486379b025ee626fa57d
JuanAlvap <alvarezjp@uninorte.edu.co>
2024-11-22 23:18:12 -0500

    Nothing

--- src/core/controllers/Validator.java
+++ src/core/controllers/Validator.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package core.controllers;
 
 import core.validate.NumberValidation;
 import core.validate.StringValidation;
 
-/**
- *
- * @author cande
- */
 public class Validator {
     public boolean validate(StringValidation v, String text){
         return v.validate(text);
